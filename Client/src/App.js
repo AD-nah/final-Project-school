925a2720b5956f4615f137c3272e48c68261923f
AD-nah <46660199+AD-nah@users.noreply.github.com>
2020-02-23 20:59:54 +0100

    ...

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react'
 import './App.css';
 import Nav from '../src/Components/Nav';
-<<<<<<< HEAD
-
-=======
->>>>>>> 885a8523d6d6041b811641a5d472011feb5cd273
 import Footer from "./Components/Footer/Footer"
 export default class App extends Component {
   render() {
