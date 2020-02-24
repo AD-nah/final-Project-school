5db83415016b500533786b9d497eee8266332c4b
AD-nah <46660199+AD-nah@users.noreply.github.com>
2020-02-24 01:51:37 +0100

    /api/auth-req  created

--- Server/Server.js
+++ Server/Server.js
@@ -3,8 +3,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var server = express();
 
@@ -14,7 +12,8 @@ server.use(express.urlencoded({ extended: false }));
 server.use(cookieParser());
 server.use(express.static(path.join(__dirname, 'public')));
 
-server.use('/', indexRouter);
-server.use('/users', usersRouter);
+server.use('/', require('./routes/index'));
+server.use('/users', require('./routes/users'));
+server.use('/api/auth-req',    require('./routes/auth'))
 
 module.exports = server;
