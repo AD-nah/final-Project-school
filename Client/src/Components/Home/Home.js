6d1762171aa45478f2c941006bf988467ad89a9a
wisam <wisam.basheir@gmail.com>
2020-02-24 16:17:51 +0100

    adding some Photo to the Home

--- Client/src/Components/Home/Home.js
+++ Client/src/Components/Home/Home.js
@@ -1,74 +1,98 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import img from '../../imgs/test.jpg';
+import img1 from '../../imgs/test1.jpg';
+import img2 from '../../imgs/test2.jpg';
+import img3 from '../../imgs/test3.jpg';
+
 
 export default class Home extends Component {
     render() {
         return (
             <React.Fragment>
-                <div id="carousel-example-1z" class="carousel slide carousel-fade" data-ride="carousel">
+                <div id="carousel-example-1z" className="carousel slide carousel-fade" data-ride="carousel">
 
-                    <ol class="carousel-indicators">
-                        <li data-target="#carousel-example-1z" data-slide-to="0" class="active"></li>
+                    <ol className="carousel-indicators">
+                        <li data-target="#carousel-example-1z" data-slide-to="0" className="active"></li>
                         <li data-target="#carousel-example-1z" data-slide-to="1"></li>
                         <li data-target="#carousel-example-1z" data-slide-to="2"></li>
                     </ol>
 
-                    <div class="carousel-inner" role="listbox">
+                    <div className="carousel-inner" role="listbox">
 
-                        <div class="carousel-item active">
-                            <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(130).jpg"
+                        <div className="carousel-item active">
+                            <img className="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(130).jpg"
                                 alt="First slide" />
                         </div>
 
-                        <div class="carousel-item">
-                            <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(129).jpg"
+                        <div className="carousel-item">
+                            <img className="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(129).jpg"
                                 alt="Second slide" />
                         </div>
 
-                        <div class="carousel-item">
-                            <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg"
+                        <div className="carousel-item">
+                            <img className="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg"
                                 alt="Third slide" />
                         </div>
 
                     </div>
 
-                    <a class="carousel-control-prev" href="#carousel-example-1z" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
+                    <a className="carousel-control-prev" href="#carousel-example-1z" role="button" data-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="sr-only">Previous</span>
                     </a>
-                    <a class="carousel-control-next" href="#carousel-example-1z" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
+                    <a className="carousel-control-next" href="#carousel-example-1z" role="button" data-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="sr-only">Next</span>
                     </a>
 
                 </div>
                 <br />
                 <br />
                 {/* Cards Code start 24,02,2020*/}
-                <div class="container">
+              
+                <div className="row">
+                    <div className="col">
+                       
+                       
+                        <img src={img} alt="Testig Photo"/>
+                    </div>
+
+                    <div className="col">
+                    <h2>Ambitioni dedisse scripsisse iudicaretur. Cras mattis iudicium purus sit amet fermentum. Donec sed odio operae, eu vulputate felis rhoncus. Praeterea iter est quasdam res quas ex communi. At nos hinc posthac, sitientis piros Afros. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Cras mattis iudicium purus sit amet fermentum.</h2>
+                    </div>
+
+                 
+
+                </div>
+                <div className="row">
+                    <div className="col mt-4">
+                       
+                    <h2>Ambitioni dedisse scripsisse iudicaretur. Cras mattis iudicium purus sit amet fermentum. Donec sed odio operae, eu vulputate felis rhoncus. Praeterea iter est quasdam res quas ex communi. At nos hinc posthac, sitientis piros Afros. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Cras mattis iudicium purus sit amet fermentum.</h2>
+                        
+                    </div>
+
+                    <div className="col">
+                    <img src={img1} alt="Testig Photo"/>
+                    
+                    </div>
+                    </div>
+
+                {/*Third Div 3*/}
+                <div className="row mt-3">
+                    <div className="col">
+                        <img src={img2}/>
+                    </div>
+                    <div className="col">
+                        <img src={img3}/>
+                    </div>
+
+                </div>
+
+                {/*SECUND DEV*/}
+
                     {/*grid  system */}
                     {/* <!-- Stack the columns on mobile by making one full-width and the other half-width --> */}
-                    <div class="media">
-  <img class="d-flex mr-3" src="https://mdbootstrap.com/img/Photos/Others/placeholder4.jpg" alt="Generic placeholder image">
-  <div class="media-body">
-    <h5 class="mt-0 font-weight-bold">Media heading</h5>
-    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in
-    vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia
-    congue felis in faucibus.
-
-    <div class="media mt-4">
-      <a class="d-flex pr-3" href="#">
-        <img src="https://mdbootstrap.com/img/Photos/Others/placeholder6.jpg" alt="Generic placeholder image"/>
-      </a>
-      <div class="media-body">
-        <h5 class="mt-0 font-weight-bold">Media heading</h5>
-        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in
-        vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia
-        congue felis in faucibus.
-      </div>
-    </div>
-  </div>
-</div>
-
+ 
                     {/* <div class="row">
                         
                         <div class="col-sm">
@@ -149,7 +173,7 @@ export default class Home extends Component {
 
                             </div>    </div>
                     </div> */}
-                </div>
+                
                 <br />
                 <br />
                 {/* Cards Code ende 20,02,2020*/}
