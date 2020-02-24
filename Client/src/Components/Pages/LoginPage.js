f8b3a6f998740cd5a4da43410f37ab333343f38f
AD-nah <46660199+AD-nah@users.noreply.github.com>
2020-02-24 01:33:37 +0100

    map "loginAction()" to props (Redux)

--- Client/src/Components/Pages/LoginPage.js
+++ Client/src/Components/Pages/LoginPage.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react'
 import LoginForm from '../Forms/LoginForm'
-export default class LoginPage extends Component {
+import * as reduxActions from '../../Redux/Actions/auth'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+
+class LoginPage extends Component {
 
     render() {
 
         const submit = (data) => {
             console.log(data)
-            return this.props.loginAction(userLoginAuthData).then(() => this.props.history.push('/'))
-
+            return this.props.loginAction(data).then(() => this.props.history.push('/'))
         }
+        
         return (
                 <LoginForm submit = {submit} />
         )
     }
 }
+
+LoginPage.propTypes = {
+    history: PropTypes.shape({
+     push: PropTypes.func.isRequired
+   }).isRequired,
+   loginAction: PropTypes.func.isRequired,
+};
+export default connect(null, { loginAction: reduxActions.loginAction })(LoginPage)
+
