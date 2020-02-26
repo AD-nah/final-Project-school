c07dff211c86239deceb42244b692ebe6b101a90
adslemann <ad.sulemann@gmail.com>
2020-02-26 15:57:54 +0100

    Added Rate Stars + added sidebar nav

--- Client/src/Components/Products/Him/Him.js
+++ Client/src/Components/Products/Him/Him.js
@@ -1,10 +1,8 @@
 
 import React,{Component} from 'react';
-import ReactDOM from "react-dom";
 
 
 //MATERIAL-UI Library for React packages
-
 import Button from '@material-ui/core/Button/Button';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
@@ -17,12 +15,17 @@ import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import "../style.css"
 
-// the ANTD Library
+
+
+// Import Antd  library
 import "antd/dist/antd.css";
 import { Menu, Icon } from "antd";
+import { Rate } from 'antd';
+const { SubMenu } = Menu;
+const desc = ['terrible', 'bad', 'normal', 'good', 'wonderful'];
 
 
-// the style of the cards
+// the Cards style from Material-ui
 const useStyles = makeStyles(theme => ({
   icon: {
     marginRight: theme.spacing(2),
@@ -53,115 +56,174 @@ const useStyles = makeStyles(theme => ({
     backgroundColor: theme.palette.background.paper,
     padding: theme.spacing(6),
   },
+  side:{
+    maxWidth:"100px",
+    float:"left"
+  }
 }));
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-
-
-
-//  the toggle search
+const  cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 
 
 
 
-
-
-export default function Him() {
-
-  const classes = useStyles();
-
+export default class Him extends React.Component {
  
- 
+  //for the rates
+  state = {
+    value: 3,
+  };
+  // for the  rate chages
+  handleChange = value => {
+    this.setState({ value });
+  };
+
+  handleClick = e => {
+    console.log("click ", e);
+  };
+
+  render() {
+
+    // for the rates
+    const { value } = this.state;
+
+    return (
+
+      <React.Fragment>
+        <CssBaseline /> 
 
 
-
-  return (
-    <React.Fragment>
-    <CssBaseline />
-
-      
-      {/* START THE TOGGLE INFO */}
-      
-       {/* the cards contents */}
-
-
-     <div className={classes.heroContent}>
-       <Container maxWidth="sm">
-         <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-           Products for Him layout
-         </Typography>
-         <Typography variant="h5" align="center" color="textSecondary" paragraph>
-           Something short and leading about the collection below—its contents, the creator, etc.
-           Make it short and sweet, but not too short so folks don&apos;t simply skip over it
-           entirely.
-         </Typography>
-         <div className={classes.heroButtons}>
-           <Grid container spacing={2} justify="center">
-             <Grid item>
-               <Button variant="outlined" color="primary">
-                 Filter
-               </Button>
-             </Grid>
-           </Grid>
-         </div>
-       </Container>
-     </div>
-
-
-     <Container className={classes.cardGrid} maxWidth="lg">
-       {/* End hero unit */}
-       <Grid container spacing={4}>
-         {cards.map(card => (
-           <Grid item key={card} xs={12} sm={6} md={4}>
-             <Card className={classes.card}>
-               <CardMedia
-                 className={classes.cardMedia}
-                 image="https://source.unsplash.com/random"
-                 title="Image title"
-               />
-               <CardContent className={classes.cardContent}>
-                 <Typography gutterBottom variant="h5" component="h2">
-                   Heading
-                 </Typography>
-                 <Typography>
-                   This is a media card. You can use this section to describe the content.
-                 </Typography>
-                 <hr/>
-
-                 <Typography gutterBottom variant="h7" >
-                         <Typography variant="h5" component="h3">&#36;</Typography>
-                 </Typography>
-
-               </CardContent>
-
-
-               <CardActions>
-                 <Button size="small" color="primary">
-                   View
-                 </Button>
-
-
-                 <Button size="small" color="primary">
-                   Favorite +
-                 </Button>
+        {/* start side */}
+          <Container>
+             <div style={{maxWidth:"550px", float:"left"}}>
+                  <Menu
+                    onClick={this.handleClick}
+                    style={{ width: 180 }}
+                    defaultSelectedKeys={["1"]}
+                    defaultOpenKeys={["sub1"]}
+                    mode="inline"
+                  
+                  >
+                    <SubMenu
+                      key="sub1"
+                      title={
+                        <span>
+                          <Icon type="search" />
+                          <span>  Search </span>
+                        </span>
+                      }
+                    >
+                      
+                        <Menu.Item key="1">
+                            <Icon type="skin" />Clothe
+                        </Menu.Item>
+                        <Menu.Item key="2">
+                            <Icon type="yuque" />Shoes
+                        </Menu.Item>
                 
+                      
+                    </SubMenu>
+                    <SubMenu
+                      key="sub2"
+                      title={
+                        <span>
+                          <Icon type="appstore" />
+                          <span>Else</span>
+                        </span>
+                      }
+                    >
+                      <Menu.Item key="5">Option 5</Menu.Item>
+                      <Menu.Item key="6">Option 6</Menu.Item>
+                    
+                    </SubMenu>
+                   
+                  </Menu>
+                  </div>
 
-                 <Button size="small" color="primary">
-                   Chart +
-                 </Button>
+                    {/* the header */}
+            <div className={useStyles.heroContent} >
+          
+                <Typography component="h1" variant="h4" align="center" color="textPrimary" gutterBottom>
+                  Products for Him layout
+                </Typography>
 
-               </CardActions>
-             </Card>
-           </Grid>
-         ))}
-       </Grid>
-     </Container>
-         {/* end of the old contents */}
-\
-  
-  </React.Fragment>
-  )
+                <div style={{wordWrap:"break-word"}}>
+                  <Typography variant="h6" align="center" color="textSecondary" paragraph>
+                    Something short and leading about the collection below—its contents, the creator, etc.
+                    Make it short and sweet, but not too short so folks don&apos;t simply skip over it
+                    entirely.
+                  </Typography>
+                </div>
+            </div>
+          </Container>
+       {/* end side */}
+
+
+
+
+     
+     
+
+
+
+            <Container className={useStyles.cardGrid} maxWidth="lg" >
+            
+              <Grid container spacing={4}>
+                {cards.map(card => (
+                  <Grid item key={card} xs={12} sm={6} md={4}>
+                    <Card className={useStyles.card}>
+                      <CardMedia
+                        className={useStyles.cardMedia}
+                        image="https://picsum.photos/200/300"
+                        title="Image title"
+                      />
+                      <CardContent className={useStyles.cardContent}>
+                        <Typography gutterBottom variant="h5" component="h2">
+                          Heading
+                        </Typography>
+                        <Typography>
+                          This is a media card. You can use this section to describe the content.
+                        </Typography>
+
+                        <hr/>
+                        <span>
+                          <Rate tooltips={desc} onChange={this.handleChange} value={value} />
+                          {value ? <span className="ant-rate-text">{desc[value - 1]}</span> : ''}
+                        </span>
+                        <hr/>
+
+                        <Typography gutterBottom variant="h7" >
+                                <Typography variant="h5" component="h3">&#36;</Typography>
+                        </Typography>
+
+                      </CardContent>
+
+
+                      <CardActions>
+                        <Button size="small" color="primary">
+                          View
+                        </Button>
+
+
+                        <Button size="small" color="primary">
+                          Favorite +
+                        </Button>
+                        
+
+                        <Button size="small" color="primary">
+                          Chart +
+                        </Button>
+
+                      </CardActions>
+                    </Card>
+                  </Grid>
+                ))}
+              </Grid>
+            </Container>
+          </React.Fragment>        
+     
+   
+
+    );
+  }
 }
-
-
