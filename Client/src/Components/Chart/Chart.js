f6fa3f1ec01c044e003835e362bc56df2d1ab7cb
adslemann <ad.sulemann@gmail.com>
2020-02-28 14:13:48 +0100

    Added Chart Table

--- Client/src/Components/Chart/Chart.js
+++ Client/src/Components/Chart/Chart.js
@@ -1,11 +1,55 @@
-import React, { Component } from 'react'
+import React from 'react';
+import MaterialTable from 'material-table';
 
-export default class Chart extends Component {
-    render() {
-        return (
-            <div>
-              Welcome from chart  
-            </div>
-        )
-    }
+export default function Chart() {
+  const [state, setState] = React.useState({
+    columns: [
+      { title: 'Item id', field: 'Id' },
+      { title: 'Description', field: 'Description' },
+      { title: 'Colour', field: 'Colour'},
+      {title: 'Birth Place',field: 'birthCity',lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },},
+    ],
+    data: [
+      { Id: '2900', Description: 'Clothe', Colour: 'Blue', birthCity: 63 },
+      {
+        Id: '2800',
+        Description: 'Shoe',
+        Colour: 'Black',
+        birthCity: 34,
+      },
+    ],
+  });
+
+  return (
+    <MaterialTable
+      title="Your Products"
+      columns={state.columns}
+      data={state.data}
+      editable={{
+        onRowAdd: newData =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              setState(prevState => {
+                const data = [...prevState.data];
+                data.push(newData);
+                return { ...prevState, data };
+              });
+            }, 600);
+          }),
+      
+        onRowDelete: oldData =>
+          new Promise(resolve => {
+            setTimeout(() => {
+              resolve();
+              setState(prevState => {
+                const data = [...prevState.data];
+                data.splice(data.indexOf(oldData), 1);
+                return { ...prevState, data };
+              });
+            }, 600);
+          }),
+      }}
+    />
+  );
 }
