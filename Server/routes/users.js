8bd1e4ae3585865b4acb31f1c4106c6443c2e9f5
AD-nah <46660199+AD-nah@users.noreply.github.com>
2020-03-03 12:33:06 +0100

    Products Routes added

--- Server/routes/users.js
+++ Server/routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+// const Product = require('../models/Product')
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+  // women.forEach((item => {
+
+  //   const newProduce = new Product({
+  //     product_type: '5e5d1d8303249d2ee09ce759',
+  //     name: item.name,
+  //     description: item.description,
+  //     image: item.image,
+  //     price: item.price
+  //   })
+  //   newProduce.save()
+  // }))
+
 module.exports = router;
+
+
+
