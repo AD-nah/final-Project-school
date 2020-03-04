9b424431fd87006743d3efb6dc8e5b028e7f017d
adslemann <ad.sulemann@gmail.com>
2020-03-04 17:59:33 +0100

    Making Modal

--- Client/src/Components/Products/Women/Women.js
+++ Client/src/Components/Products/Women/Women.js
@@ -1,9 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
 import axios from "axios";
 import { connect } from "react-redux";
 import { addToBasketAction } from "../../../Redux/Actions/basket";
-import TestingPage from "../TestingPage";
-
+import {Button, Modal} from 'react-bootstrap'
 // ----------------------------------------------------------------------
 
 // Import Antd  library
@@ -28,7 +27,9 @@ class Women extends React.Component {
 
     this.state = {
       data: null,
-      currentProduct: []
+      currentProduct: [],
+      currentArrayOfImages : null
+
     };
   }
 
@@ -44,6 +45,11 @@ class Women extends React.Component {
     this.props.addToBasketAction(item);
   }
 
+  sendImagesToCarousel(array){
+    console.log(array)
+    this.setState({currentArrayOfImages: array})
+  }
+
   starMaker(n) {
     let stars = [];
     for (let i = 0; i < n; i++) {
@@ -54,9 +60,10 @@ class Women extends React.Component {
   render() {
     return (
       <div className="container">
-        <div>
-          <TestingPage />
-        </div>
+{/*     
+       {this.state.currentArrayOfImages && (<ImagesModal img= {this.state.currentArrayOfImages[0]} />)} */}
+
+       <Example/>
 
         <h3 className="h3">Women's Collection</h3>
         <div className="row">
@@ -79,21 +86,27 @@ class Women extends React.Component {
 
 
                         <ul className="social">
-
-                            <li><a href="#" className="fas fa-expand-arrows-alt"></a></li>
+                        <li>
+                                <a
+                                  onClick={() => this.sendImagesToCarousel(item.images)}
+                                  className="fas fa-expand-arrows-alt" 
+                                  >
+                                </a>
+                          </li>
 
                             <li><a href="#" className="far fa-heart"></a></li>
 
                             <li>
                                 <a
-                                role="button"
-                                tabIndex={0}
-                                onClick={() => this.setCurrentProduct(item)}
-                                className="fa fa-shopping-cart"
+                                  role="button"
+                                  tabIndex={0}
+                                  onClick={() => this.setCurrentProduct(item)}
+                                  className="fa fa-shopping-cart"
                                 >
-                                
                                 </a>
                           </li>
+
+
                         </ul>
 
 
@@ -121,5 +134,146 @@ class Women extends React.Component {
     );
   }
 }
+
+
+
+
+
+
+function Example() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}> show </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+        </Modal.Header>
+        <Modal.Body>
+          Woohoo, you're reading this text in a modal!
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+}
+
+
+
+class ImagesModal extends React.Component {
+  render() {
+    return (
+      <div>
+
+
+        <li>
+          <a href="#" className="fas fa-expand-arrows-alt" data-toggle="modal" data-target="#product_view">Modal</a>
+        </li>
+
+
+
+        <div class="modal fade product_view" id="product_view">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-body">
+                <div class="row">
+                 
+
+
+                    <div class="col-md-12 product_img">
+                        <div id="demo" class="carousel slide" data-ride="carousel">
+                            <ul class="carousel-indicators" >
+                                <li
+                                data-target="#demo"
+                                data-slide-to="0"
+                                class="active"
+                                ></li>
+                                <li data-target="#demo" data-slide-to="1"></li>
+                                <li data-target="#demo" data-slide-to="2"></li>
+                            </ul>
+
+                            <div
+                                class="carousel-inner"
+                                
+                            >
+                                <div class="carousel-item active">
+
+                                <img
+                                    src= {this.props.img}
+                                    alt="Los Angeles"
+                                    width="1100"
+                                    height="500"
+                                />
+                                </div>
+{/* 
+                                <div class="carousel-item">
+                                <img
+                                    src=
+                                    alt="Chicago"
+                                    width="1100"
+                                    height="500"
+                                />
+                               
+                                </div>
+                                <div class="carousel-item">
+                                <img
+                                    src=
+                                    alt="New York"
+                                    width="1100"
+                                    height="500"
+                                />
+                                
+                                </div> */}
+                            </div>
+
+
+                            <a
+                                class="carousel-control-prev"
+                                href="#demo"
+                                data-slide="prev">
+                                    
+                                <i class="fas fa-backward" style={{fontSize:"40px",color:"white"}}></i>
+
+                            </a>
+                            <a
+                                class="carousel-control-next"
+                                href="#demo"
+                                data-slide="next"
+                            >
+                            <i class="fas fa-forward" style={{fontSize:"40px",color:"white"}}></i>
+                            </a>
+                        </div>
+                    </div>
+                    {/* DONT DELETE IT PLEASE */}
+                        {/* <h3 class="cost">
+                        <span class="glyphicon glyphicon-usd"></span> 15.00{" "}
+                        <small class="pre-cost">
+                            <span class="glyphicon glyphicon-usd"></span> 20.00
+                        </small>
+                     </h3> */}
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
 
 export default connect(null, { addToBasketAction })(Women);
