04a8db79741e85a935edb1799fb353f00c5240c4
adslemann <ad.sulemann@gmail.com>
2020-03-05 14:25:10 +0100

    ..

--- Client/src/Components/Products/Women/Women.js
+++ Client/src/Components/Products/Women/Women.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BackTop } from 'antd';
+import { BackTop } from "antd";
 
 import axios from "axios";
 import { connect } from "react-redux";
@@ -34,7 +34,7 @@ import {
   /* DON'T DELETE IT PLEASE */
 }
 
-  // Import Antd  library
+// Import Antd  library
 // import "antd/dist/antd.css";
 // import {  Popconfirm, message} from "antd";
 
@@ -77,18 +77,19 @@ class Women extends React.Component {
       .get("/product/womens")
       .then(res => res.data.women)
       .then(product => {
+        console.log(product)
         this.setState({ data: product });
       });
   }
 
   setCurrentProduct(item) {
     this.props.addToBasketAction(item);
+    alert("Added to basket")
   }
 
   sendImagesToCarousel(array) {
     console.log(array);
     this.setState({ currentArrayOfImages: array });
-   
   }
 
   starMaker(n) {
@@ -100,167 +101,162 @@ class Women extends React.Component {
   }
 
   render() {
-    return (<>
-      <div className="container">
-      
+    return (
+      <>
+        <div className="container">
+          <h3 className="h3">Women's Collection</h3>
+          <div className="row">
+            {this.state.data
+              ? this.state.data.map((item, index) => {
+                  return (
+                    <div key={index} className="col-md-3 col-sm-6">
+                      <div className="product-grid7">
+                        <div className="product-content">
+                          <h3 className="title">
+                            <a href="#">{item.name}</a>
+                          </h3>
+                        </div>
 
-        <h3 className="h3">Women's Collection</h3>
-        <div className="row">
-          {this.state.data
-            ? this.state.data.map((item, index) => {
-                return (
-                  <div key={index} className="col-md-3 col-sm-6">
-                    <div className="product-grid7">
-                      <div className="product-content">
-                        <h3 className="title">
-                          <a href="#">{item.name}</a>
-                        </h3>
-                      </div>
+                        <div className="product-image7">
+                          <a href="#">
+                            
+                          {/*<img className="pic-1" src={item.images[0]} />
+                          <img className="pic-2" src={item.images[1]} /> */}
 
-                      <div className="product-image7">
-                        <a href="#">
-                          <img className="pic-1" src={item.images[0]} />
-                          <img className="pic-2" src={item.images[1]} />
-                        </a>
 
-                        <ul className="social">
-                          
-                          <li onClick={() =>
+                          {/* 
+                            <img
+                              className="pic-1"
+                              src={item.images.protoTypes[0]}
+                            />
+                            <img
+                              className="pic-2"
+                              src={item.images.protoTypes[1]}
+                            /> */}
+                          </a>
+
+                          <ul className="social">
+                            <li
+                              onClick={() =>
                                 this.sendImagesToCarousel(item.images)
-                              }>
-                            <a
-                              onClick={this.toggle(13)}
-                              className="fas fa-expand-arrows-alt"
-                            ></a>
-                          </li>
+                              }
+                            >
+                              <a
+                                onClick={this.toggle(13)}
+                                className="fas fa-expand-arrows-alt"
+                              ></a>
+                            </li>
 
-                          <li>
-                            <a href="#" className="far fa-heart"></a>
-                          </li>
+                            <li>
+                              <a href="#" className="far fa-heart"></a>
+                            </li>
 
-                          <li>
-                            <a
-                              role="button"
-                              tabIndex={0}
-                              onClick={() => this.setCurrentProduct(item)}
-                              className="fa fa-shopping-cart"
-                            ></a>
-                          </li>
-                        </ul>
+                            <li>
+                              <a
+                                role="button"
+                                tabIndex={0}
+                                onClick={() => this.setCurrentProduct(item)}
+                                className="fa fa-shopping-cart"
+                              ></a>
+                            </li>
+                          </ul>
 
-                        <span className="product-new-label">New</span>
-                      </div>
-                      <div className="product-content">
-                        <h3 className="title">
-                          <a href="#">{item.description}</a>
-                        </h3>
-                        <ul className="rating">
-                          {this.starMaker(item.rating)}
-                        </ul>
-                        <div className="price">
-                          {item.prices[0]}
-                          <span>{item.prices[1]}</span>
+                          <span className="product-new-label">New</span>
+                        </div>
+                        <div className="product-content">
+                          <h3 className="title">
+                            <a href="#">{item.description}</a>
+                          </h3>
+                          <ul className="rating">
+                            {this.starMaker(item.rating)}
+                          </ul>
+                          <div className="price">
+                            {item.prices[0]}
+                            <span>{item.prices[1]}</span>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                );
-              })
-            : "Please wait ..."}
+                  );
+                })
+              : "Please wait ..."}
+          </div>
         </div>
-      </div>
 
+        {/* Back to top btn */}
+        <div>
+          <BackTop>
+            <i
+              className="fas fa-chevron-circle-up"
+              style={{ color: "orange", fontSize: "40px" }}
+            ></i>
+          </BackTop>
+        </div>
 
+        {/* // images modal */}
+        <div>
+          <MDBContainer>
+            <li>
+              <a
+                onClick={this.toggle(13)}
+                className="fas fa-expand-arrows-alt"
+              ></a>
+            </li>
 
+            <MDBModal isOpen={this.state.modal13} toggle={this.toggle(13)}>
+              <MDBModalBody>
+                {/* modal carousel starthere */}
+                <MDBContainer>
+                  <MDBCarousel
+                    activeItem={1}
+                    length={2}
+                    showControls={true}
+                    showIndicators={true}
+                    className="z-depth-1"
+                  >
+                    <MDBCarouselInner>
+                      <MDBCarouselItem itemId="1">
+                        <MDBView>
+                          <img
+                            className="d-block w-180"
+                            src="https://mdbootstrap.com/img/Photos/Slides/img%20(68).jpg"
+                            alt="First slide"
+                          />
+                          <MDBMask overlay="black-light" />
+                        </MDBView>
 
+                        <MDBCarouselCaption>
+                          <h3 className="h3-responsive">Light mask</h3>
+                          <p>First text</p>
+                        </MDBCarouselCaption>
+                      </MDBCarouselItem>
 
-          {/* Back to top btn */}
-         <div>
-                <BackTop>
-                
-                <i className="fas fa-chevron-circle-up" style={{color:"orange",fontSize:"40px",}}></i>
-                </BackTop>
-         </div>
-
-
-
-
-     
-
-      {/* // images modal */}
-      <div>
-      <MDBContainer>
-        <li>
-          <a
-            onClick={this.toggle(13)}
-            className="fas fa-expand-arrows-alt"
-          ></a>
-        </li>
-
-        <MDBModal isOpen={this.state.modal13} toggle={this.toggle(13)}>
-          <MDBModalBody>
-            {/* modal carousel starthere */}
-            <MDBContainer>
-              <MDBCarousel
-                activeItem={1}
-                length={2}
-                showControls={true}
-                showIndicators={true}
-                className="z-depth-1"
-              >
-
-
-                <MDBCarouselInner>
-
-
-                  <MDBCarouselItem itemId="1">
-                    <MDBView>
-                      <img
-                        className="d-block w-180"
-                        src="https://mdbootstrap.com/img/Photos/Slides/img%20(68).jpg"
-                        alt="First slide"
-                      />
-                      <MDBMask overlay="black-light" />
-                    </MDBView>
-
-                    <MDBCarouselCaption>
-                      <h3 className="h3-responsive">Light mask</h3>
-                      <p>First text</p>
-                    </MDBCarouselCaption>
-                  </MDBCarouselItem>
-
-
-
-                  <MDBCarouselItem itemId="2">
-                    <MDBView>
-                      <img
-                        className="d-blockw-180"
-                        src="https://mdbootstrap.com/img/Photos/Slides/img%20(6).jpg"
-                        alt="Second slide"
-                      />
-                      <MDBMask overlay="black-strong" />
-                    </MDBView>
-                    <MDBCarouselCaption>
-                      <h3 className="h3-responsive">Strong mask</h3>
-                      <p>Second text</p>
-                    </MDBCarouselCaption>
-                  </MDBCarouselItem>
-
-
-                 
-                </MDBCarouselInner>
-              </MDBCarousel>
-            </MDBContainer>
-            {/* modal carousel end here */}
-          </MDBModalBody>
-        </MDBModal>
-      </MDBContainer>
-    </div>
-    {/*images modal end here */}
-
-    </>);
+                      <MDBCarouselItem itemId="2">
+                        <MDBView>
+                          <img
+                            className="d-blockw-180"
+                            src="https://mdbootstrap.com/img/Photos/Slides/img%20(6).jpg"
+                            alt="Second slide"
+                          />
+                          <MDBMask overlay="black-strong" />
+                        </MDBView>
+                        <MDBCarouselCaption>
+                          <h3 className="h3-responsive">Strong mask</h3>
+                          <p>Second text</p>
+                        </MDBCarouselCaption>
+                      </MDBCarouselItem>
+                    </MDBCarouselInner>
+                  </MDBCarousel>
+                </MDBContainer>
+                {/* modal carousel end here */}
+              </MDBModalBody>
+            </MDBModal>
+          </MDBContainer>
+        </div>
+        {/*images modal end here */}
+      </>
+    );
   }
 }
 
 export default connect(null, { addToBasketAction })(Women);
-
