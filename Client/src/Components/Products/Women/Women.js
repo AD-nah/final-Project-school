b538f6928c8638eda191d23fc76dd1e12005328e
adslemann <ad.sulemann@gmail.com>
2020-03-06 14:45:02 +0100

    Animated Css div Added

--- Client/src/Components/Products/Women/Women.js
+++ Client/src/Components/Products/Women/Women.js
@@ -16,6 +16,8 @@ import {
   MDBMask
 } from "mdbreact";
 
+import ImageZoomAnim from "../../../imgs/cadinfluencer1.jpg";
+
 // ----------------------------------
 
 {
@@ -77,7 +79,7 @@ class Women extends React.Component {
       .get("/product/womens")
       .then(res => res.data.women)
       .then(product => {
-        console.log(product)
+        console.log(product);
         this.setState({ data: product });
       });
   }
@@ -85,8 +87,7 @@ class Women extends React.Component {
   setCurrentProduct(item) {
     this.props.addToBasketAction(item);
 
-    alert('added to Basket')      
-
+    alert("added to Basket");
   }
 
   sendImagesToCarousel(array) {
@@ -105,49 +106,54 @@ class Women extends React.Component {
 
   render() {
     return (
-
-
       <>
         <div className="container">
           <h3 className="h3">Women's Collection</h3>
+
+          <div className="row">
+            <h4 class="wordCarousel">
+              <span>Why Scount ? </span>
+              <div>
+                <ul class="flip4">
+                  <li>Best Quality</li>
+                  <li>New Trends</li>
+                  <li>Free Shipping</li>
+                  <li>Satisfiction </li>
+                </ul>
+              </div>
+            </h4>
+          </div>
+
+
+
+          <div className="space-ten">
+          </div>
+
           <div className="row">
             {this.state.data
               ? this.state.data.map((item, index) => {
                   return (
                     <div key={index} className="col-md-3 col-sm-6">
-
-
-              
-                      <div className="product-grid7"> 
-                        
-
-                      <div className="product-content">
+                      <div className="product-grid7">
+                        <div className="product-content">
                           <h3 className="title">
                             <a href="#">{item.name}</a>
                           </h3>
-                      </div>
+                        </div>
 
-              
-
-
-                      <hr/>
+                        <hr />
                         <div className="product-image7">
                           <a href="#">
-
-                      
-                             <img
+                            <img
                               className="pic-1"
-                              style={{maxHeight:"300px"}}
+                              style={{ maxHeight: "300px" }}
                               src={item.images.protoTypes[0]}
                             />
 
-
-                           <img
+                            <img
                               className="pic-2"
                               src={item.images.protoTypes[1]}
-                           />  
-
-
+                            />
                           </a>
 
                           <ul className="social">
@@ -177,29 +183,24 @@ class Women extends React.Component {
                           </ul>
 
                           <span className="product-new-label">New</span>
-
                         </div>
-                        
 
                         <div className="product-content">
                           <h3 className="title">
                             <a href="#">{item.description}</a>
                           </h3>
 
-                        
-
                           <ul className="rating">
                             {this.starMaker(item.rating)}
                           </ul>
                           <div className="price">
-                              &#8364; 
+                            &#8364;
                             {item.prices[0]}
                             <span>{item.prices[1]}</span>
                           </div>
 
-                          <hr/>
+                          <hr />
                         </div>
-
                       </div>
                     </div>
                   );
@@ -221,9 +222,12 @@ class Women extends React.Component {
         {/* // images modal */}
         <div>
           <MDBContainer>
-          
-            <MDBModal isOpen={this.state.modal13} toggle={this.toggle(13)} className="w-auto p-3">
-              <MDBModalBody >
+            <MDBModal
+              isOpen={this.state.modal13}
+              toggle={this.toggle(13)}
+              className="w-auto p-3"
+            >
+              <MDBModalBody>
                 {/* modal carousel starthere */}
                 <MDBContainer>
                   <MDBCarousel
@@ -234,33 +238,28 @@ class Women extends React.Component {
                     className="z-depth-1"
                   >
                     <MDBCarouselInner>
+                      {this.state.currentArrayOfImages.length > 0
+                        ? this.state.currentArrayOfImages.map((item, index) => {
+                            return (
+                              <MDBCarouselItem itemId={index + 1}>
+                                <MDBView>
+                                  <img
+                                    className="d-block"
+                                    style={{ maxHeight: "550px" }}
+                                    src={item}
+                                    alt="First slide"
+                                  />
+                                  <MDBMask overlay="black-light" />
+                                </MDBView>
 
-                      {this.state.currentArrayOfImages.length> 0?
-
-                          this.state.currentArrayOfImages.map((item,index)=>{
-                              return (
-                                <MDBCarouselItem itemId={index+1}>
-                                  <MDBView>
-                                    <img
-                                      className="d-block"
-                                      style={{maxHeight:"550px"}}
-                                      src={item}
-                                      alt="First slide"
-                                    />
-                                    <MDBMask overlay="black-light" />
-                                  </MDBView>
-
-                                  {/* <MDBCarouselCaption>
+                                {/* <MDBCarouselCaption>
                                     <h3 className="h3-responsive">Light mask</h3>
                                     <p>First text</p>
                                   </MDBCarouselCaption> */}
-                                </MDBCarouselItem>
-                              )
+                              </MDBCarouselItem>
+                            );
                           })
-                          :null
-                      }
-
-                      
+                        : null}
                     </MDBCarouselInner>
                   </MDBCarousel>
                 </MDBContainer>
@@ -274,6 +273,5 @@ class Women extends React.Component {
     );
   }
 }
-
 
 export default connect(null, { addToBasketAction })(Women);
