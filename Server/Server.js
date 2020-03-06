ad6f85db1643b24e621e72a24166cc4a3f11e94d
ad-nah <ahmad.alnahlawi@icloud.com>
2020-03-06 07:44:38 +0200

    authentiacation messages handeled

--- Server/Server.js
+++ Server/Server.js
@@ -31,7 +31,7 @@ server.use(express.static(path.join(__dirname, 'public')));
 server.use('/', require('./routes/index'));
 server.use('/users', require('./routes/users'));
 server.use('/api/auth-req',    require('./routes/auth'))
-server.use('/api/logout-req',    require('./routes/logout'))
+// server.use('/api/logout-req',    require('./routes/logout'))
 server.use('/api/register-req',    require('./routes/register'))
 
 server.use('/product', require('./routes/product'));
