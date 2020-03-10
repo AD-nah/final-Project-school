0520b52025dbba63cf386c2711d63db86ee3dc71
AD-nah <46660199+AD-nah@users.noreply.github.com>
2020-03-10 12:45:45 +0100

    combine productReducer to reduxState

--- Client/src/Redux/combineReducers.js
+++ Client/src/Redux/combineReducers.js
@@ -2,7 +2,9 @@ import { combineReducers }from 'redux'
 
 import userReducer from './Reducers/userReducer'
 import basketReducer from './Reducers/basketReducer'
+import productReducer from './Reducers/productReducer'
 export default combineReducers({
     userReducer,
-    basketReducer
+    basketReducer,
+    productReducer
 })
