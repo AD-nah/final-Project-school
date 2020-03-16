d4cca32d14356d08255cffed4ba4cf7c27dc36c2
AD-nah <46660199+AD-nah@users.noreply.github.com>
2020-03-16 18:01:39 +0100

    remove from basket finish

--- Server/routes/basket.js
+++ Server/routes/basket.js
@@ -1,35 +1,80 @@
 const express = require('express');
 const router = express.Router();
 const Basket = require('../models/Basket');
-const male =  '5e5d1d5a03249d2ee09ce758';
-const female = '5e5d1d8303249d2ee09ce759';
+
 
 router.post('/get-basket', async (req, res, next) => { 
-    console.log(req.body)
+
     const { userID } = req.body
 
     const findInBasket = await Basket.findOne({user_ID: userID})
         if(findInBasket){
 
             res.json({basket: findInBasket.basket});
+
         }else{
             res.status(404).json({err:'basket not found'})
         }
+    })
+
+    router.post('/save-to-basket', async (req, res, next)=> {
+        
+    const { userID, item } = req.body
+    
+
+    const findBasketAndUpdate = await Basket.findOneAndUpdate({ user_ID: userID }, { $push: { basket : item }}).then(newRecord => {
+
+        if(newRecord){
+
+            res.json({items: newRecord.basket});
+        }
+        
+    }).catch(err => {
+        console.log(err)
+    })
+
+
+
+
+    // const handleDuplicate = await Basket.findOne({user_ID: userID}, { basket : { $elemMatch: {_id: item._id } } } ).then((f) => {
+    //     const handleDuplicate = await Basket.findOne({user_ID: userID}, { basket : item } ).then((f) => {
+
+    //     if(f){
+    //         console.log(f)
+            
+    //     }else{
+    //         console.log('!')
+    //     }
+
+    // }).catch(err => {
+    //     console.log(err)
+    // })
+
+
+    // if(handleDuplicate){
+
+    //     res.status(404).json({items: 'product is already exsist!'})
+
+    // }else{
+
+        // const findBasketAndUpdate = await Basket.findOneAndUpdate({ user_ID: userID }, { $push: { basket : item }})
+        // if(findBasketAndUpdate){
+        //     res.json({items: findBasketAndUpdate.basket});
+        // }
+    // }
 })
 
-router.post('/save-to-basket', async (req, res, next)=> {
+
+router.post('/remove-from-basket',async (req, res, next) => {
 
     const { userID, item } = req.body
 
-    const findBasketAndUpdate = await Basket.findOneAndUpdate({ user_ID: userID }, { $push: { basket : item }})
+    const removeingItem = await Basket.update({ user_ID : userID }, { $pull : { basket : { _id : item._id}}})
+    if(removeingItem){
+        res.status(200).json({item: 'removed from basket'})
+    }
 
-        if(findBasketAndUpdate){
-            res.json({items: findBasketAndUpdate.basket});
-        }else{
-            res.status(404).json({err:'basket not found'})
-        }
 })
-
 
 
 
