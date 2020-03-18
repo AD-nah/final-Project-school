7914a108882138242d81b64058e98b344db940eb
adsulemann <ad.sulemann@gmail.com>
2020-03-18 17:41:38 +0100

    Getting images to women carousel in Home Page

--- Client/src/Components/Home/Home.js
+++ Client/src/Components/Home/Home.js
@@ -1,5 +1,9 @@
 import React, { Component, Link } from 'react';
 import img from '../../imgs/test.jpg';
+import { fetchWomenProducts } from '../../Redux/Actions/products'
+import { fetchMenProducts } from '../../Redux/Actions/products'
+import { connect } from "react-redux";
+
 
 import dressbox from './Boxes images/women with dressbox.jpg';
 import './home.css';
@@ -11,8 +15,6 @@ import caro3 from './imgCarousel/d-women1.jpg';
 import caro4 from './imgCarousel/d-men2.jpg';
 import caro5 from './imgCarousel/d-men3.jpg';
 import caro6 from './imgCarousel/d-women3.jpg';
-
-
 import BoxIMmage from './Boxes images/128143675--624x394.jpg';
 
 
@@ -28,8 +30,31 @@ import { BackTop } from 'antd';
 
 
 
-export default class Home extends Component {
+ class Home extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       women:[],
+       men:[]
+    }
+  }
+  
+  componentDidMount() {
+    this.props.fetchWomenProducts().then(res => {
+      this.setState({women: this.props.getWomenProducts.womenProducts})
+    })
+
+    this.props.fetchMenProducts().then(res => {
+      this.setState({men: this.props.getMenProducts.menProducts})
+    })
+}
+
+
+
   render() {
+
     return (
       <>
 
@@ -129,7 +154,7 @@ export default class Home extends Component {
 
 
 
-        {/*shop women Products Carousel end */}
+        {/*shop women Products Carousel sta */}
         <div class="my-5 text-center container">
           <div class="womenbox" >
             <p className='heartbeat'>Women Shop</p>
@@ -152,15 +177,42 @@ export default class Home extends Component {
                     <div class="row">
 
                       <div class="col-12 col-md d-flex align-items-center justify-content-center">
-                        First Pic
+                      {this.state.women.length > 0?
+                            <img
+                            className="pic-1"
+                            style={{ maxHeight: "400px" }}
+                            src={this.state.women[0].images.protoTypes[0]}
+                          />
+                    
+                      :
+                      <h1>Loading image...</h1>
+                    }
                     </div>
 
                       <div class="col-12 col-md d-flex align-items-center justify-content-center">
-                        Second Pic
+                      {this.state.women.length > 0?
+                            <img
+                            className="pic-1"
+                            style={{ maxHeight: "400px" }}
+                            src={this.state.women[1].images.protoTypes[0]}
+                          />
+                    
+                      :
+                      <h1>Loading iamge...</h1>
+                    }
                     </div>
 
                       <div class="col-12 col-md d-flex align-items-center justify-content-center" class="col-12 col-md d-flex align-items-center justify-content-center">
-                        Third Pic
+                      {this.state.women.length > 0?
+                            <img
+                            className="pic-1"
+                            style={{ maxHeight: "400px" }}
+                            src={this.state.women[2].images.protoTypes[0]}
+                          />
+                    
+                      :
+                      <h1>Loading iamge...</h1>
+                    }
                     </div>
                     </div>
                   </div>
@@ -169,15 +221,42 @@ export default class Home extends Component {
                     <div class="row">
 
                       <div class="col-12 col-md d-flex align-items-center justify-content-center">
-                        Forth Pic
+                      {this.state.women.length > 0?
+                            <img
+                            className="pic-1"
+                            style={{ maxHeight: "400px" }}
+                            src={this.state.women[3].images.protoTypes[0]}
+                          />
+                    
+                      :
+                      <h1>Loading iamge...</h1>
+                    }
                       </div>
 
                       <div class="col-12 col-md d-flex align-items-center justify-content-center">
-                        Fifth Pic
+                      {this.state.women.length > 0?
+                            <img
+                            className="pic-1"
+                            style={{ maxHeight: "400px" }}
+                            src={this.state.women[4].images.protoTypes[0]}
+                          />
+                    
+                      :
+                      <h1>Loading iamge...</h1>
+                    }
                       </div>
 
                       <div class="col-12 col-md d-flex align-items-center justify-content-center" class="col-12 col-md d-flex align-items-center justify-content-center">
-                        Sixth Pic
+                      {this.state.women.length > 0?
+                            <img
+                            className="pic-1"
+                            style={{ maxHeight: "400px" }}
+                            src={this.state.women[5].images.protoTypes[0]}
+                          />
+                    
+                      :
+                      <h1>Loading</h1>
+                    }
                       </div>
 
                     </div>
@@ -454,3 +533,14 @@ export default class Home extends Component {
     )
   }
 }
+
+
+const  mapStateToProps = (state) => {
+  return {
+    getWomenProducts: state.productReducer,
+    getMenProducts : state.productReducer
+
+  }
+}
+
+export default connect(mapStateToProps, {fetchMenProducts, fetchWomenProducts })(Home);
