c2b9a270b893abf099bc0f0b87b5ee190000bf51
wisam <wisam.basheir@gmail.com>
2020-03-18 11:51:32 +0100

    not finsh from Outdit scond hanjsd

--- Client/src/Components/Products/Outfits/Outfits.js
+++ Client/src/Components/Products/Outfits/Outfits.js
@@ -24,17 +24,39 @@ function cancel(e) {
 
 
 export default class Outfits extends React.Component {
-  
+
 
   render() {
 
 
     return (
-       
+
        <div className="container">
-           Outfits
+           Second-Hand
          </div>              
-                      
+
     );
   }
 }
+
+
+// import React, { Component } from 'react';
+// import './secondhand.css';
+
+// export default class Outfits extends React.Component {
+//   render() {
+//     return (
+//       <div className="container">
+//         <div className="overlay-slide col-md-12">
+//           <img src="https://via.placeholder.com/220x280" />
+//           <div>
+//             <h2>Awesome Title</h2>
+//             <p>This hover effect is totally awesome.</p>
+//           </div>
+
+//         </div>
+
+//       </div>
+//     )
+//   }
+// }
