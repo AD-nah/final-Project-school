3609ae1e914c8178b65713a4e029a5e5351e1108
adslemann <ad.sulemann@gmail.com>
2020-03-24 17:19:07 +0100

    Products sidenav bar modified

--- Client/src/Components/Products/Products.js
+++ Client/src/Components/Products/Products.js
@@ -2,40 +2,81 @@ import React, { Component } from 'react';
 import Men from './Men/Men';
 import Women from './Women/Women';
 import Reworked from './Reworked/Reworked';
-import { Switch, Route } from 'react-router-dom';
-import { MDBNav, MDBNavLink, MDBNavItem } from "mdbreact";
+import { Switch, Route, Link } from 'react-router-dom';
 
+import '../Products/Products.css'
 
 export default class Products extends Component {
     render() {
-        return (< div className="container" >
-            <div className="row" >
-                <div className='col-sm-12 col-md-4 col-lg-2' >
-                    <div class="sidebar" >
-                        <MDBNav className="nav-fill" >
+        return (
 
-                            <MDBNavItem >
-                                <MDBNavLink to="/products/women" > Women 's</MDBNavLink>
-                            </MDBNavItem >
-                            <MDBNavItem >
-                                <MDBNavLink to="/products/men" > Men 's </MDBNavLink> 
-                            </MDBNavItem >
-                            <MDBNavItem >
-                                <MDBNavLink to="/products/reworked" > Reworked </MDBNavLink> </MDBNavItem >
-                            </MDBNav>
+
+            <div class="container-fluid ">
+                <div class="row">
+
+                    <div class="products-sidebar">
+                        <Link to="/products/women" className="fa fa-venus "> Women </Link>
+                        <Link to="/products/men" className="fa fa-mars"> Men</Link>
+                        <Link to="/products/reworked" className="fa fa-cut"> Reworked</Link>
+
+                        <li >
+                            <a class="collapsed  fa fa-caret-down" href="#submenu1" data-toggle="collapse" data-target="#submenu1">More
+                            </a>
+                            <div class="collapse" id="submenu1" aria-expanded="false">
+                                <ul class="flex-column nav">
+                                    <li ><a class="py-0" href="#"><span>Orders</span></a></li>
+                                    <li >
+                                        <a class="collapsed fa fa-caret-down" href="#submenu1sub1" data-toggle="collapse" data-target="#submenu1sub1"><span>Profile</span></a>
+                                        <div class="collapse" id="submenu1sub1" aria-expanded="false">
+                                            <ul class="flex-column nav ">
+                                                <li >
+                                                    <Link>
+                                                        <i class="fa fa-fw fa-clock-o"></i>Activity
+                                                    </Link>
+                                                </li>
+                                                <li >
+                                                    <Link>
+                                                        <i class="far fa-address-card"></i> Dashboard
+                                                    </Link>
+                                                </li>
+                                                <li >
+
+                                                    <Link>
+                                                        <i class="fa fa-fw fa-bar-chart"></i> Charts
+                                                    </Link>
+                                                </li>
+                                                <li >
+                                                    <Link>
+                                                        <i class="fa fa-fw fa-compass"></i>Areas
+                                                    </Link>
+
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+
+
                     </div>
-                </div>
-                <div className='col-sm-12 col-md-10 col-lg-10' >
-                    <div >
+
+
+
+
+                    <div className='col-sm-12 col-md-10 col-lg-10' >
                         <Switch >
                             <Route path="/products/reworked" component={Reworked} />
                             <Route path="/products/men" component={Men} />
                             <Route path="/products/women" component={Women} />
                         </Switch >
+
                     </div>
+
+
+
                 </div>
             </div>
-        </div>
         )
     }
 }
