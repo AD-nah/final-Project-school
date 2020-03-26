10d33b5aa2f2bdf87e927528fe056c84ad561c00
adslemann <ad.sulemann@gmail.com>
2020-03-26 17:35:06 +0100

    Reject Duplicated items inside the Baskit

--- Server/routes/basket.js
+++ Server/routes/basket.js
@@ -10,36 +10,63 @@ router.post('/get-basket', async (req, res, next) => {
 
 console.log(userId)
 
-    const findInBasket = await Basket.findOne({userId: userId})
+const findInBasket = await Basket.findOne({userId: userId})
 
-        if(findInBasket){
+    if(findInBasket){
 
-            res.json({basket: findInBasket.basket});
+        res.json({basket: findInBasket.basket});
 
-        }else{
-            res.status(404).json({err:'basket not found'})
-        }
-    })
+    }else{
+        res.status(404).json({err:'basket not found'})
+    }
+})
 
-    router.post('/save-to-basket', async (req, res, next)=> {
-        
+router.post('/save-to-basket', async (req, res, next)=> {
+
     const { userId, item } = req.body
+
+    let duplicated = false;
+
+    let basket = await Basket.findOne({userId:userId});
+    console.log(basket)
+    if (basket.basket.length > 0) {
+        basket.basket.forEach(product=>{
+            if(item._id===product._id){
+                duplicated = true;
+            }
+        })
+    }
+
+    if ( duplicated ){
+        res.send({msg: "Product already exist!!!"})
+    }else{
+
+        Basket.findOneAndUpdate({ userId: userId }, { $push: { basket : item }}).then(response=>{
+            res.send({msg: "Product has been added to basket!"})
+        }).catch(error=>{
+            res.status(400).send({msg: error})
+        })
+
+    }
+
+
+
     
-    const findBasketAndUpdate = await Basket.findOneAndUpdate({ userId: userId }, { $push: { basket : item }}).then(newRecord => { // check for duplicate 
+    // const findBasketAndUpdate = await Basket.findOneAndUpdate({ userId: userId }, { $push: { basket : item }}).then(newRecord => { // check for duplicate 
 
-        // if(duplicated){ 
-        //      res.json({error : 'product is already exist!'})
-        // } 
+    //     // if(duplicated){ 
+    //     //      res.json({error : 'product is already exist!'})
+    //     // } 
 
-        if(newRecord){
+    //     if(newRecord){
 
-            res.json({items: newRecord.basket});
+    //         res.json({items: newRecord.basket});
 
-        }
+    //     }
         
-    }).catch(err => {
-        console.log(err)
-    })
+    // }).catch(err => {
+    //     console.log(err)
+    // })
 
 
 
@@ -75,12 +102,12 @@ console.log(userId)
 
 router.post('/remove-from-basket', async (req, res, next) => {
 
-    const { userId, item } = req.body
+const { userId, item } = req.body
 
-    const removeingItem = await Basket.update({ userId : userId }, { $pull : { basket : { _id : item._id}}})
-    if(removeingItem){
-        res.status(200).json({item: 'removed from basket'})
-    }
+const removeingItem = await Basket.update({ userId : userId }, { $pull : { basket : { _id : item._id}}})
+if(removeingItem){
+    res.status(200).json({item: 'removed from basket'})
+}
 
 })
 
