af9270ca8d06aca003179dafe8920f8e60a36a1e
adslemann <ad.sulemann@gmail.com>
2020-03-27 13:11:46 +0100

    Sell page was modified

--- Client/src/Components/Profile/Sell/Sell.js
+++ Client/src/Components/Profile/Sell/Sell.js
@@ -1,12 +1,17 @@
 import React from "react";
 import './sell.css';
-import sellImg from './sellImg/sell.jpeg';
-import percent from './sellImg/50-percent.jpg';
-import s from './sellImg/s.webp'
+
+
+
+
+
 class Sell extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { file: '', imagePreviewUrl: '' };
+        this.state = { file: '', imagePreviewUrl: '',
+        file2:'',imagePreviewUrl2:'',
+        file3:'',imagePreviewUrl3:'',
+        };
     }
 
     _handleSubmit(e) {
@@ -17,92 +22,158 @@ class Sell extends React.Component {
 
     _handleImageChange(e) {
         e.preventDefault();
-
         let reader = new FileReader();
-        let file = e.target.files[0];
-
+        let file=e.target.files[0];
+       
         reader.onloadend = () => {
             this.setState({
                 file: file,
-                imagePreviewUrl: reader.result
+                imagePreviewUrl: reader.result, 
             });
         }
-
         reader.readAsDataURL(file)
     }
 
-    render() {
+    _handleImageChange2(e) {
+        e.preventDefault();
+        let reader = new FileReader();
+        let file2=e.target.files[0];
+       
+        reader.onloadend = () => {
+            this.setState({
+                file2: file2,
+                imagePreviewUrl2: reader.result, 
+            });
+        }
+        reader.readAsDataURL(file2)
+    }
 
-        let { imagePreviewUrl } = this.state;
-        let $imagePreview = null;
-        if (imagePreviewUrl) {
-            $imagePreview = (<img className="halloImage m-2" src={imagePreviewUrl} style={{ width: "20vw", height: "40vh" }} />);
+
+    _handleImageChange3(e) {
+        e.preventDefault();
+        let reader = new FileReader();
+        let file3=e.target.files[0];
+       
+        reader.onloadend = () => {
+            this.setState({
+                file3: file3,
+                imagePreviewUrl3: reader.result, 
+            });
+        }
+        reader.readAsDataURL(file3)
+    }
+
+
+
+    render() {
+        let {imagePreviewUrl} = this.state;
+        let {imagePreviewUrl2} = this.state;
+        let {imagePreviewUrl3} = this.state;
+
+        let $imagePreview=null;
+        let $imagePreview2=null;
+        let $imagePreview3=null;
+       
+
+        if (imagePreviewUrl || imagePreviewUrl2 || imagePreviewUrl3 ==='') {
+            $imagePreview = (<img  src={imagePreviewUrl} style={{width:"20vw",height:"18vh",padding:"2px",margin:'2px' }} />);
+            $imagePreview2 = (<img  src={imagePreviewUrl2} style={{width:"20vw",height:"18vh",padding:"2px",margin:'2px' }} />);
+            $imagePreview3 = (<img  src={imagePreviewUrl3} style={{width:"20vw",height:"18vh",padding:"2px",margin:'2px' }} />);
+           
+           
         } else {
             $imagePreview = (<div className="previewText">Please select an Image for Preview</div>);
-
         }
 
         return (
-            <div className="container regFormContainer font-weight-bold">
-                <div className="row m-3" /*style={{ backgroundColor: "orange" }}*/>
-                    <div className="col-md-4" /*style={{ backgroundColor: "#ddd" }}*/>
-                        <h3 className="row-md-2 font-whigt-bold text-center font-weight-bold">Big brands, small prices</h3>
-                        <img className="img-fluid row-md-2 row-sm-2" src={sellImg} style={{ width: "15vw", height: "30vh" }} />
-                    </div>
-                    <div className="col-md-4" style={{ backgroundColor: "" }}>
+            <div className="container Sell-container font-weight-bold">
+                <div className="row m-2 " style={{ backgroundColor: "#ffebd3" }}>
+               
+                    <div className="row m-2">
+                        <div className=" row-md-4 img-thumbnail mx-auto d-block ">
+                            {$imagePreview}
+                            {$imagePreview2}
+                            {$imagePreview3}
+                            
+                        </div>
+                        <div className="row-md-6 ml-5">
+                            <form onSubmit={(e) => this._handleSubmit(e)}>
 
-                    <h2 className="text-center font-whigt-bold">    Exclusive offers:
-                                Register Login below</h2>
-                        <img src={percent} style={{ width: "20vw" }} />
-                       
-                    </div>
-                    <div className="col-md-4" style={{ width: "30vw" }}>
-                        <img src={s} style={{ width: "20vw"}}/>
-                        
-                         </div>
-
-                </div>
-                <div className="row m-2 " style={{backgroundColor:"#d5d4d3"}}>
-                    {/* <div className="row-md-2">
-
-                </div> */}
-                <div className="row m-2">
-                    <div className=" row-md-4 img-thumbnail mx-auto d-block ">
-                        {$imagePreview}
-                    </div>
-                    <div className="row-md-6 ml-5">
-                        <form onSubmit={(e) => this._handleSubmit(e)}>
-                            <div className="form-group">
-                                <label for="exampleFormControlInput1">Product Name</label>
-                                <input type="name" className="form-control" id="exampleFormControlInput1" placeholder="product name" />
-                            </div>
-                            <div className="form-group">
-                                <label for="exampleFormControlInput1">Price</label>
-                                <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Inter your Price" />
-
-                            </div>
-
-                            <div className="form-group">
-                                <label for="exampleFormControlTextarea1">Write your product description</label>
-                                <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                            </div>
+                                <label for="product_name">Poduct Name</label>
+                                <div class="form-group styled-select">
+                                  
+                                    <input id="product_name" name="product_name" placeholder="Product Name.." class="form-control input-md" required="" type="text" style={{backgroundColor:""}}/>
+                                  
+                                </div>
 
 
+                                <label for="product_categorie">Category</label>
+                                <div class="form-group styled-select">
+                                    <select>
+                                        <option>Select Category</option>
+                                        <option>The second option</option>
+                                        <option>The thrid option</option>
+                                    </select><span class="fa fa-sort-desc"></span>
+                                </div>
 
 
-                            <input className="fileInput" style={{ backgroundColor: "#a1a8a5" }}
-                                type="file"
-                                onChange={(e) => this._handleImageChange(e)} />
-                            {/* <button className="submitButton"
-                                type="submit"
-                                onClick={(e) => this._handleSubmit(e)}>Upload Image</button> */}
+                                <label for="available_quantity">Quantity</label>
+                                <div class="form-group styled-select">
+                                    <select>
+                                        <option>Select Quantity</option>
+                                        <option>The second option</option>
+                                        <option>The thrid option</option>
+                                    </select><span class="fa fa-sort-desc"></span>
+                                </div>
+
+
+                                <label for="available_quantity">Size</label>
+                                <div class="form-group styled-select">
+                                    <select>
+                                        <option>Select a size</option>
+                                        <option>The second option</option>
+                                        <option>The thrid option</option>
+                                    </select><span class="fa fa-sort-desc"></span>
+                                </div>
+
+                                <label for="percentage_discount">Discount</label>
+                                <div class="form-group styled-select">
+                                    <select>
+                                        <option>Percenage</option>
+                                        <option>The second option</option>
+                                        <option>The thrid option</option>
+                                    </select><span class="fa fa-sort-desc"></span>
+                                </div>
+
+
+
+                                <div class="form-group">
+                                    <label for="product_description">Prroduct Description</label>
+                                    <textarea class="form-control" id="product_description" name="product_description"></textarea>
+                                </div>
 
 
 
 
+                                <div class="form-group">
+                                    <label for="filebutton">Image 3</label> 
+                                    <input onChange={(e) => this._handleImageChange(e)} id="filebutton" name="filebutton" class="input-file" type="file" />
 
-                        </form>
-                    </div>
+                                    <label for="filebutton">Image 2</label> 
+                                    <input onChange={(e) => this._handleImageChange2(e)} id="filebutton" name="filebutton" class="input-file" type="file" />
+
+                                    <label for="filebutton">Image 3</label> 
+                                    <input onChange={(e) => this._handleImageChange3(e)} id="filebutton" name="filebutton" class="input-file" type="file" />
+                                </div>
+
+
+                                <button className="submitButton btn btn-outline-transparent"
+                                    type="submit"
+                                    onClick={(e) => this._handleSubmit(e)}>Save Product</button>
+
+
+                            </form>
+                        </div>
                     </div>
                 </div>
 
