4a4679ec78656bca9341fe9a30458f7e3c017ffe
adslemann <ad.sulemann@gmail.com>
2020-03-31 17:28:31 +0200

    Back to top responsive fixed

--- Client/src/App.js
+++ Client/src/App.js
@@ -11,16 +11,16 @@ import { BackTop } from 'antd';
 export default class App extends Component {
   render() {
     return (
-        <div  className="App" style = {{marginTop: '35px'}} >
-          <Nav/>
-            {/* Back to top btn */}
-
-        <BackTop style={{ backgroundColor: 'grey', opacity: '0.8', color: 'white', fontSize: '20px', width: "6%", textAlign: 'center', fontWeight: '700' }}>
-          Scroll top
+      <div className="App" style={{ marginTop: '35px' }} >
+        <Nav />
+   
+          {/* Back to top btn */}
+          <BackTop style={{ backgroundColor: 'grey', opacity: '0.8', color: 'white', fontSize: '18px', width: "7%", height: "5%", textAlign: 'center', fontWeight: '700',borderRadius:"2px" }}>
+          <span>Top</span>
         </BackTop>
-
-          <Footer/>
-        </div>
+      
+        <Footer />
+      </div>
     )
   }
 }
