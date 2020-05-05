a4439c3c49e055194dc56e72c0327c87903c6733
AD-nah <46660199+AD-nah@users.noreply.github.com>
2020-05-05 06:03:40 +0200

    repo cleaned

--- routes/parseErrors.js
+++
@@ -1,9 +0,0 @@
-const _ = require('lodash')
-
-module.exports = (errors) => {
-    const result = {}
-    _.forEach(errors, (val, key)=>{
-        result[key] = val.message
-    })
-    return result
-}
